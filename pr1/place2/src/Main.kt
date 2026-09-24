9b5ff80ec8f2d161d9c76253fa02887f00b92685
Casl39 <141814091+Casl39@users.noreply.github.com>
2026-09-24 17:01:55 +0000

    Add files via upload

---
+++ pr1/place2/src/Main.kt
@@ -0,0 +1,6 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val string = readln()
+    print(string.groupingBy { it }.eachCount())
+}
